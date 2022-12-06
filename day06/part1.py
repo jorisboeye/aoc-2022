a17d7164b1e11b31c5db19a395a8397839a3384a
Joris Boeye <joris.boeye@zf.com>
2022-12-06 22:23:52 +0100

    Solve day06.

---
+++ day06/part1.py
@@ -0,0 +1,34 @@
+from collections import deque
+from pathlib import Path
+
+import pytest
+
+FILE = Path(__file__)
+TEST_RESULT = 10
+TEST_INPUT = """nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"""
+
+
+def solve(puzzle_input: str) -> int:
+    message = deque["str"]([], maxlen=14)
+    for idx, c in enumerate(puzzle_input):
+        message += c
+        if len(message) == 4 == len(set(message)):
+            return idx + 1
+    return 0
+
+
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    ((TEST_INPUT, TEST_RESULT),),
+)
+def test(test_input: str, expected: int) -> None:
+    assert solve(test_input) == expected
+
+
+if __name__ == "__main__":
+    test_answer = solve(puzzle_input=TEST_INPUT)
+    print(test_answer)
+    if test_answer == TEST_RESULT:
+        with open(FILE.parent / "input.txt") as file:
+            answer = solve(puzzle_input=file.read())
+        print(answer)
