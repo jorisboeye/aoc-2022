4077c122017b3f4a3e247850f3ac8e9b65fb2f2c
Joris Boeye <joris.boeye@zf.com>
2022-12-06 22:00:01 +0100

    Update day01.

--- day01/part1.py
+++ day01/part1.py
@@ -1,9 +1,8 @@
 def main(path: str) -> int:
     with open(path) as file:
-        txt = file.read()
-    print(max(sum(map(int, e.strip().split("\n"))) for e in txt.split("\n\n")))
+        print(max(sum(map(int, e.splitlines())) for e in file.read().split("\n\n")))
     return 0
 
 
 if __name__ == "__main__":
-    raise SystemExit(main("./day01/input-1.txt"))
+    raise SystemExit(main("./day01/input.txt"))
